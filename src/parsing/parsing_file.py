4b744ed4617c97da0724e06ec2777694e328abb6
Cathya Andrianjaka <cathyaandrianjaka@gmail.com>
2026-08-16 19:48:38 +0300

    Update parsing_file.py

--- src/parsing/parsing_file.py
+++ src/parsing/parsing_file.py
@@ -1,4 +1,9 @@
 def parse_file(name: str) -> list[str]:
     with open(name, "r") as f:
-        content = file.read(name)
+        content = file.readlines()
+    return [line.split("\n") for line in content]
+
+
+def parse_content_file(content: list[str]):
+    
     

50b1a8787fcfcd8a6445a5a8a2697317bda42e3f
Hanintsoa Cathya Edith Andrianjaka <haniandr@c1r10s11.42antananarivo.mg>
2026-08-27 17:09:19 +0300

    add a new comments

--- src/gen_func_name.py
+++ src/gen_func_name.py
@@ -18,9 +18,6 @@ structural JSON format with the names, the user's request, the arguments with th
 The functions: {func_name}
 
 request: "{query}"
-output: {
-        "name": "fn_add_numbers",
-        "arguments": {"a": 3.0, "b": 2.0}"
 Result:
 
 """
@@ -33,13 +30,18 @@ class GenerationFuncName:
         self._valid_pos = 0
         self.result = []
 
-    def get_first_arg(self, request) -> None | list[..., ...]:
+    def get_name_key(self, request) -> None | list[..., ...]:
+        """
+        Get the name key 
+        """
         # combine the prompt and the function name needed 
         # inside with the user's query 
-        prompt = template.format(func_name="".join(func_name), query=request)
+        self.prompt = template.format(func_name="".join(func_name), query=request)
+
+        first_key_token = self._model.encode(first_arg).tolist()[0]
+
         while True:
-            input_ids = self._model.encode(prompt)
-            first_key_token = self._model.encode(first_arg).tolist()[0]
+            input_ids = self._model.encode(self.prompt)
             logits = self._model.get_logits_from_input_ids(
                 input_ids.tolist()[0]
             )
@@ -51,11 +53,41 @@ class GenerationFuncName:
             if argmax == first_key_token[self._valid_pos]:
                 self._valid_pos += 1
                 self.result.append(valid_token)
-            prompt += valid_token
+            self.prompt += valid_token
             if self._valid_pos == len(first_key_token):
                 break
+        self.result = "".join(self.result)
         return self.result
 
 
-gen = GenerationFuncName()
-print(gen.get_first_arg("What is the sum of 3 and 2?" ))
+    # def get_name_value(self) -> None:
+    #     index = 0
+    #     func_tok_value = self._model.encode(func_name)
+        
+    #     if self.result:
+    #         input_ids = self._model.encode(self.prompt)
+    #         logits = self._model.get_logits_from_input_ids(
+    #             input_ids()[0]
+    #         )
+            
+    #         # sort the logits by their value but return the index
+    #         sorted_logits = sorted(
+    #                 range(len(logits)),
+    #                 key=lambda x: logits[x],
+    #                 reverse=True,
+    #         )
+
+            # name_supposed = ""            
+            # for i in range(len(sorted_logits[:100])):
+            #     max_token = self._model.decode(sorted_logits[i])
+            #     for name in func_tok_value:
+            #         if max_token in name:
+            #             name_supposed += name
+            #             index = len(max_token)
+                    
+
+
+
+if __name__ == "__main__":
+    gen = GenerationFuncName()
+    print(gen.get_name_key("What is the sum of 3 and 2?" ))

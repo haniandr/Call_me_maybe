6741af20fc5af5de8c9c668a5a8d00c71285064f
Hanintsoa Cathya Edith Andrianjaka <haniandr@c1r10s11.42antananarivo.mg>
2026-09-04 15:36:10 +0300

    add the class NUMBERS AND STRINGS for the fsm

--- src/extract_params.py
+++ src/extract_params.py
@@ -1,26 +1,58 @@
-from llm_sdk import Small_LLM_Model
-from gen_func_name import GenerationFuncName
+from enum import Enum, auto
+from abc import ABC, abstractmethod
+from typing import Any
 
 
-template = """
-The task is to generate the arguments value
-expected according to the type of function and 
-the types of arguments.
+prompt = "Replace all numbers in \"Hello I'm 34 bla bla\" with NUMBERS"
 
-query: "{query}"
-The arguments type with its function:
-{func_type}
-
+class State(str, Enum):
+    START = auto()
+    WAIT_QUOTE = auto()
+    CONTENT = auto()
+    END = auto()
 
 
-"""
+class TYPE_ARG(ABC):
+    @abstractmethod
+    def take_state(state: str, content: Any) -> None:
+        ...
 
-class GenerationArguments:
-    def __init__(self) -> None:
-        self._model = Small_LLM_Model()
-        self.arguments = []
-        self.prompt = None
 
-    def gen_arg_value(self) -> None:
-        
+class STRING(TYPE_ARG):
+    def take_state(state: str, char: str) -> str:
+        if char == ' ':
+            return state
+        if state == "START":
+            if char == ' ':
+                return "START"
+            else:
+                return "STRING"
+        elif state == "STRING":
+            return "WAIT_QUOTE" if char else None
+        elif state == "WAIT_QUOTE":
+            return "END" if char == '"' else None
+        return None
 
+
+class NUMBER(TYPE_ARG):
+    def take_state(state: str, char: Any) -> None:
+        if state == "START":
+            if char == '-' or char == '+':
+                return "SIGN"
+            return "NUMBERS"
+        elif state == "SIGN":
+            return "NUMBERS"
+        elif state == "NUMBERS":
+            if char == ".":
+                return "COMMA"
+            elif char.isdigit():
+                return "WAIT_QUOTE"
+            else:
+                return None
+        if state == "COMMA":
+            return "DECIMAL" if char else None
+        if state == "DECIMAL":
+            if char.isdigit():
+                return "WAIT_QUOTE"
+            return None
+        return None

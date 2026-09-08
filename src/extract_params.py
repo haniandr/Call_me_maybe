ba5f8da2d24fae5ad8ea571684f1b48bf5561981
Hanintsoa Cathya Edith Andrianjaka <haniandr@c1r10s11.42antananarivo.mg>
2026-09-08 15:28:56 +0300

    add a class Constraint with 2 methods that generate the parameters and combine all the process to return a dict

--- src/extract_params.py
+++ src/extract_params.py
@@ -2,6 +2,8 @@ from enum import Enum, auto
 from abc import ABC, abstractmethod
 from typing import Any
 from llm_sdk import Small_LLM_Model
+from src.parsing.parsing_function import Parsing
+from src.gen_func_name import GenerationFuncName
 
 
 class State(str, Enum):
@@ -14,22 +16,33 @@ class State(str, Enum):
     END = auto()
 
 
-prompt = ""
+prompt = """
+Extract the right parameters for each functions 
+following the type matched for the functions/
+Choose well which type and value should fill the parameter's value.
+
+The functions with their appropriate arguments:
+{func_param_list}
+
+parameters:
+"{param_key}":  
+"""
 
 class TYPEARG(ABC):
     @abstractmethod
-    def take_state(state: State, char: Any) -> None | State:
+    def take_state(self, state: State, char: Any) -> None | State:
         ...
 
-    def func_verify(self, state: State, content: str) -> None:
+    def func_verify(self, state: State, content: str) -> None | State:
         for char in content:
-            if self.take_state(state, char) is None:
+            state = self.take_state(state, char)
+            if state is None:
                 return None
         return state
 
 
 class STRING(TYPEARG):
-    def take_state(state: State, char: Any) -> State | None:
+    def take_state(self, state: State, char: Any) -> State | None:
         if state == State.START:
             return State.STRING
         if state == State.STRING:
@@ -40,7 +53,7 @@ class STRING(TYPEARG):
 
 
 class NUMBER(TYPEARG):
-    def take_state(state: State, char: Any) -> None | State:
+    def take_state(self, state: State, char: Any) -> None | State:
         if state == State.START:
             if char in "-+":
                 return State.SIGN
@@ -51,16 +64,16 @@ class NUMBER(TYPEARG):
         if state == State.NUMBER:
             if char == ".":
                 return State.COMMA
-            if char == '"':
-                return State.END
             elif char.isdigit():
                 return State.NUMBER
+            else:
+                return State.END
         if state == State.COMMA and char.isdigit():
                 return State.DECIMAL
         if state == State.DECIMAL:
             if char.isdigit():
                 return State.DECIMAL
-            if char == '"':
+            else:
                 return State.END
         return None
 
@@ -71,38 +84,111 @@ class BOOLEAN(TYPEARG):
 
 class ConstraintParams:
     def __init__(self) -> None:
-        self.prompt = prompt
+        self.prompt = ""
         self._model = Small_LLM_Model()
         self._arg = TYPEARG()
+        self._func = GenerationFuncName()
+
+    def get_param_func(self) -> list[dict[str, Any]]:
+        func_list = Parsing().parse_file("data/input/functions_calling_definition.json")
+        needed_list = []
+        for func in func_list:
+            needed_list.append({
+                "name": func.name,
+                "parameters": {
+                    key: value
+                    for key, value in func.parameters.items()
+                }
+            })
+        return needed_list
 
     def choose_type_state(self, name: str) -> None:
         if name == "string":
             self._arg = STRING()
+            self.prompt += '"'
         elif name == "number":
             self._arg = NUMBER()
         elif name == "boolean":
             self._arg = BOOLEAN()
+        elif name == "integer" or name == "float":
+            self._arg = NUMBER()
 
-    def extract_params(self, all_params) -> State:
-        for param in all_params.key():
-            self.prompt += f'"{param}": '
-            state = State.START
-            while state != State.END:
-                input_ids = self._model.encode(self.prompt)
-                logits = self._model.get_logits_from_input_ids(
-                    input_ids.tolist()[0]
+    def generate_param(self) -> str:
+        """
+        Generate only the parameters by the LLM model.
+        """
+        result = ""
+        state = State.START
+        while state != State.END:
+            input_ids = self._model.encode(self.prompt)
+            logits = self._model.get_logits_from_input_ids(
+                input_ids.tolist()[0]
+            )
+            sorted_logits = sorted(
+                range(len(logits)),
+                key=lambda x: logits[x],
+                reverse=True
+            )
+
+            for token in sorted_logits:
+                word = self._model.decode(token)
+                current_state = self._arg.func_verify(state, word)
+                if current_state is None:
+                    continue
+                self.prompt += word
+                state = current_state
+                result += word
+                break
+        return result
+
+
+    def combine_param(self, name_func: str) -> dict[str, Any]:
+        """
+        Combine all of the process to get the param's value
+        following the appropriate type according
+        to the function name.
+
+        Args:
+            name_func: name of the function obtained by the LLM
+        """
+        func_list = self.get_param_func()
+
+        all_params = {}
+        for func in func_list:
+            all_params[func["name"]] = func["parameters"]
+
+        param_result = {}
+        for func_name, param in all_params.items():
+            if func_name != name_func:
+                continue
+            for param_key, param_type in param.items():
+                self.prompt = prompt.format(
+                    func_param_list=all_params,
+                    param_key=param_key
                 )
-                sorted_logits = sorted(
-                    range(len(logits)),
-                    key=lambda x: logits[x],
-                    reverse=True,
-                )
-                for token in sorted_logits:
-                    word = self._model.decode(token)
-                    self.choose_type_state(all_params[param])
-                    current_state = self._arg.func_verify(state, word)
-                    if current_state is None:
-                        continue
-                    self.prompt += token + ","
-                    state = current_state
+                self.choose_type_state(param_type.lower())
+                param = self.generate_param()
+                if param:
+                    param_result[param_key] = "".join(param)
+        return param_result
+
+            #state = State.START
+                #while state != State.END:
+                #input_ids = self._model.encode(self.prompt)
+                #logits = self._model.get_logits_from_input_ids(
+                #   input_ids.tolist()[0]
+                #)
+                #sorted_logits = sorted(
+                #   range(len(logits)),
+                #    key=lambda x: logits[x#],
+                #   reverse=True,
+                #)
+                #for token in sorted_logits:
+                #   word = self._model.decode(token)
+                #    self.choose_type_state(all_params[param])
+            #   current_state = self._arg.func_verify(state, word)
+                #   if current_state is None:
+                #       continue
+                #   self.prompt += token + ","
+                #   state = current_state
 

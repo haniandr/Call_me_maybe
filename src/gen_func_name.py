16937793057a292643028be7ba8cdeb3569148dc
Hanintsoa Cathya Edith Andrianjaka <haniandr@c1r10s11.42antananarivo.mg>
2026-09-15 15:36:13 +0300

    redefine the function get_name_value

--- src/gen_func_name.py
+++ src/gen_func_name.py
@@ -1,79 +1,51 @@
+from typing import Any
 from llm_sdk import Small_LLM_Model
 
 
-func_name = [
-    "fn_add_numbers",
-    "fn_greet",
-    "fn_reverse_string",
-    "fn_get_square_root",
-    "fn_substitute_string_with_regex"
-]
-
-first_arg = '"name": '
-
-template = """
-You are a function-calling assistant. Your task is to translate the user's request into a 
-structural JSON format with the names, the user's request, the arguments with their values.
-
-The functions: {func_name}
-
-request: "{query}"
-Result:
-"name": 
-
-"""
+func_name = {
+    "fn_add_numbers": "Add two numbers together and return their sum",
+    "fn_greet": "Generate a greeting message for a person by name",
+    "fn_reverse_string": "Reverse a string and return the reversed result",
+    "fn_get_square_root": "Calculate square root of the number",
+    "fn_substitute_string_with_regex": "Replace all" 
+        "occurrences matching a regex pattern in a string"
+}
 
 
 class GenerationFuncName:
     def __init__(self) -> None:
         self._model = Small_LLM_Model()
-        self._valid_pos = 0
+        self.prompt = ""
 
-    # def get_name_key(self, request) -> None | list[str]:
-    #     """
-    #     Get the name key.
-    #     """
-    #     # combine the prompt and the function name needed
-    #     # inside with the user's query
-    #     self.prompt = template.format(
-    #         func_name="".join(func_name),
-    #         query=request
-    #     )
+    def create_prompt(
+        self,
+        query: str,
+        functions: dict[str, str]
+    ) -> str:
+        new = "".join(f" - {name}: {description}\n" for name, description in functions.items())
+        return f"""Give the appropriate function name \
+according to the user's query.
 
-    #     first_key_token = self._model.encode(first_arg).tolist()[0]
+query: {query}
 
-    #     while True:
-    #         input_ids = self._model.encode(self.prompt)
-    #         logits = self._model.get_logits_from_input_ids(
-    #             input_ids.tolist()[0]
-    #         )
-    #         for i in range(len(logits)):
-    #             if i != first_key_token[self._valid_pos]:
-    #                 logits[i] = float('-inf')
-    #         argmax = logits.index(max(logits))
-    #         if argmax == first_key_token[self._valid_pos]:
-    #             valid_token = self._model.decode(argmax)
-    #             self._valid_pos += 1
-    #             self.result.append(valid_token)
-    #         self.prompt += valid_token
-    #         if self._valid_pos == len(first_key_token):
-    #             break
-    #     # self.result = "".join(self.result)
-    #     return self.result
+Functions:
+{new}
 
-    def get_name_value(self, request) -> None | list[str]:
-        self.prompt = template.format(
-            func_name="".join(func_name),
-            query=request
-        )
-        func_tok_value = [
-            self._model.encode(f'{candidate}').tolist()[0]
-            for candidate in func_name
-        ]
-        candidates = func_tok_value
-
-        pos = 0
+name: """
+    def get_name_value(self, request) -> dict[str, Any]:
+        i = 0
         result = []
+
+        self.prompt = self.create_prompt(
+            query=request,
+            functions=func_name
+        )
+        print(self.prompt)
+
+        function_token = [
+            self._model.encode(name).tolist()[0]
+            for name in func_name
+        ]
 
         while True:
             input_ids = self._model.encode(self.prompt)
@@ -81,51 +53,36 @@ class GenerationFuncName:
                 input_ids.tolist()[0]
             )
 
-            # sort the logits by their value but return the index
-            sorted_logits = sorted(
-                    range(len(logits)),
-                    key=lambda x: logits[x],
-                    reverse=True,
-            )
-
-            # token attendu pour chq tour dans les func_token
-            expected_token = [
-                tokens[pos]
-                for tokens in candidates
-                if pos < len(tokens)
+            candidates = [
+                name[i]
+                for name in function_token
+                if i < len(name) and name[:i] == result
             ]
+            print(candidates)
 
-            chosen = None
+            if not candidates:
+                break
 
-            for token in sorted_logits:
-                if token in expected_token:
-                    chosen = token
-                    break
+            score = float('-inf')
+            token = 0
 
-            if chosen is None:
-                return None
+            for id in candidates:
+                if logits[id] > score:
+                    token = id
+                    score = logits[id]
 
-            gotten_name = self._model.decode(chosen)
-            result.append(gotten_name)
-            self.prompt += gotten_name
+            result.append(token)
+            word = self._model.decode(token)
+            self.prompt += word
+            i += 1
 
-            # sort the name_func to reduce the unuseful name
-            new_func_name = [
-                needed
-                for needed in candidates
-                if pos < len(needed) and needed[pos] == chosen
-            ]
-            candidates = new_func_name
+            # for function in function_token:
+            #     if function == result:
+            #         break
 
-            if len(candidates) == 1 and pos == len(candidates[0]) - 1:
-                if candidates[0][pos] == chosen:
-                    break
-
-            pos += 1
-
-        return "".join(result)
+        return {"name": self._model.decode(result).strip()}
 
 
 if __name__ == "__main__":
      gen = GenerationFuncName()
-     print(gen.get_name_value("Greet Shrek"))
+     print(gen.get_name_value("Hello world reverse"))

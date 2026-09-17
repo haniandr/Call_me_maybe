e0296d38e0f70624c603ccb732c4639345812542
Hanintsoa Cathya Edith Andrianjaka <haniandr@c1r10s11.42antananarivo.mg>
2026-09-17 15:27:47 +0300

    add the model class for function validation

---
+++ src/parsing/model.py
@@ -0,0 +1,54 @@
+from pydantic import (
+    BaseModel,
+    Field,
+    model_validator,
+    ValidationError
+)
+
+
+class TypeSpecify(Enum):
+    NUMBER = "number"
+    STRING = "string"
+    BOOLEAN = "boolean"
+    INTEGER = "integer"
+    FLOAT = "float"
+
+
+class ParamsType(BaseModel):
+    type: TypeSpecify
+
+
+class ReturnType(BaseModel):
+    type: TypeSpecify
+
+
+class FunctionCallingTest(BaseModel):
+    """
+    For the validation of the prompt.
+    """
+
+    prompt: str = Field()
+
+
+class FunctionResult(BaseModel):
+    """
+    Validate the result format if it contains the required
+    keys with the excepted type
+    """
+
+    prompt: str = Field()
+    name: str = Field()
+    parameters: dict[str, ParamsType] = Field()
+
+
+class FunctionDefinition(BaseModel):
+    """
+    Validate the function definition type 
+    with its exact keys and type each
+    """
+    name: str = Field()
+    description: str = Field()
+    parameters: dict[str, ParamsType] = Field()
+    returns: ReturnType
+
+
